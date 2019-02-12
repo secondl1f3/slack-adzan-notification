5b9ca3dcea1a90e775e8c5ef746edb45429a2b78
Rio Bastian <altanovela@gmail.com>
2019-02-12 17:42:23 +0700

    refactor code

--- src/test/main.js
+++ src/test/main.js
@@ -1,5 +1,5 @@
 // Testing Unit
 var tools = require("../tools");
 
-console.log(tools.subtractMinuteCron("15:05", 10));
-console.log(tools.subtractMinuteCron("05:05", 10));
+console.log(tools.subtractMinute("15:05", 10));
+console.log(tools.subtractMinute("05:05", 10));
