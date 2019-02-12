a035ac1cb0376c027a736d3f7cbab6ece1167831
Rio Bastian <altanovela@gmail.com>
2019-02-12 17:14:45 +0700

    refactor code

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@
  * author : rio.bastian@metraplasa.co.id
  * created : 2017-12-07 20:09
  */
-var cron    = require('cron');
+var cron    = require('cron').CronJob;
 var tools   = require('./tools');
 var request = require('sync-request');
 var concat  = require('concat-stream');
@@ -17,7 +17,7 @@ var t_subuh;
 
 // Constant, Minutes before sending notification
 const REMIND_IN_MINUTE = 10;
-const SLACK_CHWEB_HOOK = 'https://hooks.slack.com/services/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'
+const SLACK_CHWEB_HOOK = 'https://hooks.slack.com/services/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';
 const ADZAN_PUBLIIC_WS = 'http://api.aladhan.com/timingsByCity?city=Jakarta&country=ID&method=2';
 
 var PrayerTimes = {
@@ -41,12 +41,18 @@ var PrayerTimes = {
             json: { "text" : note }
         });
     },
-    // Centralize Message Generator
-    generateMsg : function(t, n){
+    // Message Generator
+    message : function(t, n){
         return  "<!channel> \n" +
-                "_Assalamualaikum, Ikhwan fillah,._\n" +
+                "Assalamualaikum, Ikhwan fillah,.\n" +
                 "*" + REMIND_IN_MINUTE + "* menit lagi masuk waktu " + n + ", yuk siap siap sholat\n" +
                 "Waktu " + n + " hari ini pukul *" + t.hours + ":" + t.minutes + " WIB*";
+    },
+    // Notify generated Message
+    doTask : function(adzanTimes, adzanLabel){
+        var msg = PrayerTimes.message(adzanTimes, adzanLabel);
+        console.log(new Date() + " - " + msg);
+        PrayerTimes.notify(msg);
     }
 };
 
@@ -56,82 +62,66 @@ PrayerTimes.reload();
 /**
  * Crawl Adzan times every midnight
  */
-var JobPrayerTimes = new cron.CronJob({
+new cron({
     cronTime: '00 30 01 * * *',
     onTick: function() {
         PrayerTimes.reload();
     },
-    start: false
+    start : true
 });
-JobPrayerTimes.start();
 
 /**
- * Send Sholat Notification to slack 10 minutes before Sholat Dzuhur
+ * Job Sholat Dzuhur
  */
-var JobNotifyPrayerTimesDzuhur = new cron.CronJob({
+new cron({
     cronTime: '00 ' + t_dzhur.minutes + ' ' + t_dzhur.hours + ' * * *',
     onTick: function() {
-        var msg = PrayerTimes.generateMsg(t_dzhur, "Dzuhur");
-            console.log(new Date() + " - " + msg);
-            PrayerTimes.notify(msg);
+        PrayerTimes.doTask(t_dzhur, "Dzuhur");
     },
-    start: false
+    start: true
 });
-JobNotifyPrayerTimesDzuhur.start();
 
 /**
- * Send Sholat Notification to slack 10 minutes before Sholat Ashar
+ * Job Sholat Ashar
  */
-var JobNotifyPrayerTimesAshar = new cron.CronJob({
+new cron({
     cronTime: '00 ' + t_ashar.minutes + ' ' + t_ashar.hours + ' * * *',
     onTick: function() {
-        var msg = PrayerTimes.generateMsg(t_ashar, "Ashar");
-        console.log(new Date() + " - " + msg);
-        PrayerTimes.notify(msg);
+        PrayerTimes.doTask(t_ashar, "Ashar");
     },
-    start: false
+    start: true
 });
-JobNotifyPrayerTimesAshar.start();
 
 /**
- * Send Sholat Notification to slack 10 minutes before Sholat Magrib
+ * Job Sholat Magrib
  */
-var JobNotifyPrayerTimesMagrib = new cron.CronJob({
+new cron({
     cronTime: '00 ' + t_magrb.minutes + ' ' + t_magrb.hours + ' * * *',
     onTick: function() {
-        var msg = PrayerTimes.generateMsg(t_magrb, "Magrib");
-        console.log(new Date() + " - " + msg);
-        PrayerTimes.notify(msg);
+        PrayerTimes.doTask(t_magrb, "Magrib");
     },
-    start: false
+    start: true
 });
-JobNotifyPrayerTimesMagrib.start();
 
 /**
- * Send Sholat Notification to slack 10 minutes before Sholat Isha
+ * Job Sholat Isya
  */
-var JobNotifyPrayerTimesIsha = new cron.CronJob({
+new cron({
     cronTime: '00 ' + t_ishaa.minutes + ' ' + t_ishaa.hours + ' * * *',
     onTick: function() {
-        var msg = PrayerTimes.generateMsg(t_ishaa, "Isha");
-        console.log(new Date() + " - " + msg);
-        PrayerTimes.notify(msg);
+        PrayerTimes.doTask(t_ishaa, "Isya");
     },
-    start: false
+    start: true
 });
-JobNotifyPrayerTimesIsha.start();
 
 /**
- * Send Sholat Notification to slack 10 minutes before Sholat Subuh
+ * Job Sholat Subuh
  */
-var JobNotifyPrayerTimesSubuh = new cron.CronJob({
+new cron({
     cronTime: '00 ' + t_subuh.minutes + ' ' + t_subuh.hours + ' * * *',
     onTick: function() {
-        var msg = PrayerTimes.generateMsg(t_subuh, "Subuh");
-        console.log(new Date() + " - " + msg);
-        PrayerTimes.notify(msg);
+        PrayerTimes.doTask(t_subuh, "Subuh");
     },
-    start: false
+    start: true
 });
-JobNotifyPrayerTimesSubuh.start();
 
