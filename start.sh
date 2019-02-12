b424657d2137444a253ea9759ed64541df46fd3d
Rio Bastian <altanovela@gmail.com>
2019-02-12 14:06:09 +0700

    Refactor project

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 # to detach session,
 # please uncomment disown, and run the command directly in shell console
 #
-nohup node adzan-src/main.js > apps.log 2>&1 & #disown
+nohup node src/main.js > apps.log 2>&1 & #disown
