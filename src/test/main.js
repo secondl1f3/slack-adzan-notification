af6a2c21295274a78442c4a2ba87b5587d19bf03
Rio Bastian <altanovela@gmail.com>
2019-02-14 19:06:34 +0700

    refactor, change test code

--- src/test/main.js
+++ src/test/main.js
@@ -1,5 +1,10 @@
 // Testing Unit
-var tools = require("../tools");
+var tools = require("../common");
 
-console.log(tools.subtractMinute("15:05", 10));
-console.log(tools.subtractMinute("05:05", 10));
+var adzantimes1 = tools.toTimeObject("15:05");
+var adzantimes2 = tools.toTimeObject("15:05");
+
+console.log(adzantimes1);
+console.log(tools.subtractMinute(adzantimes1, 10));
+console.log(adzantimes2);
+console.log(tools.subtractMinute(adzantimes2, 10));
